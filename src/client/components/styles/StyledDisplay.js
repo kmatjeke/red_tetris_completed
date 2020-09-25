b90e0fd9221b815d85b6cfe44ad3e51d12cea340
kmatjeke <koketsomatjeke.km@gmail.com>
2020-09-25 16:36:58 +0200

    Adding tutorials to README

--- src/client/components/styles/StyledDisplay.js
+++ src/client/components/styles/StyledDisplay.js
@@ -1,14 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledDisplay = styled.button`
-	// box-sizing: border-box;
-	// display: flex;
-	// align-items: center;
-	// min-height: 30px;
-	// font-family: Pixel, Arial, Helvetica, sans-serif;
-	// font-size: 0.8rem;
 	border-radius: 3px;
-	background: #333;
+	background: #white;
 	border: none;
 	margin: 0 0 20px 0;
 	padding: 10px;
